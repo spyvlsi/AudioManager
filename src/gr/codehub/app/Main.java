965a46b5bb46f492fdae5f4be9690daa04cb3642
spyvlsi <spyros.dellakias@gmail.com>
2020-05-04 15:16:50 +0300

    main upd

--- src/gr/codehub/app/Main.java
+++ src/gr/codehub/app/Main.java
@@ -3,6 +3,7 @@ package gr.codehub.app;
 public class Main {
     public static void main(String[] args){
         Ui ui = new Ui();
-
+        Storage storage = new Storage();
+        ui.manageStorage(storage);
     }
 }
